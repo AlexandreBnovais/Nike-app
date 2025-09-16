94018ea4a962c7ccacc696c0d50db1359138644c
AlexandreBnovais <157971881+AlexandreBnovais@users.noreply.github.com>
2025-09-16 17:39:30 -0300

    New Feature: HomeScreen

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { 
   View,
+  Image,
+  TouchableOpacity,
   Text,
-  Image
 } from 'react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { LinearGradient } from 'expo-linear-gradient'
 import { HomeStyles } from '../styles/homeStyles';
 
 export function HomeScreen() {
@@ -14,6 +16,61 @@ export function HomeScreen() {
         <AntDesign name="menu" size={24} color="black" />
         <Image source={require('../../assets/nike_732084.png')} style={HomeStyles.nikeImg}/>
         <Image source={require('../../assets/person.jpg')} style={HomeStyles.personImg}/>
+      </View>
+
+      <View style={HomeStyles.navContainer}>
+        <TouchableOpacity onPress={() => {}} style={HomeStyles.item}>
+          <Image source={require('../../assets/thumb_tennis_2.png')} style={HomeStyles.imgItem}/>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => {}} style={HomeStyles.item}>
+          <Image source={require('../../assets/thumb_tennis_1.png')} style={HomeStyles.imgItem}/>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => {}} style={HomeStyles.item}>
+          <Image source={require('../../assets/thumb_tennis_3.png')} style={HomeStyles.imgItem}/>
+        </TouchableOpacity>
+      </View>
+
+      <View style={HomeStyles.mainContainer}>
+        <View style={HomeStyles.mainImgContainer}>
+          <Image 
+            source={require('../../assets/tennis_2.png')}
+            style={HomeStyles.mainImg}
+          />
+        </View>
+        
+
+        <View style={HomeStyles.info}>
+          <Text style={HomeStyles.h1}>KYRIE INFINITY</Text>
+          <Text style={HomeStyles.h2}>Calçados para basquete</Text>
+          <Text style={HomeStyles.p}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</Text>
+        </View>
+
+        <View style={HomeStyles.footerContainer}>
+          <Text style={HomeStyles.h2}>Preço</Text>
+
+          <View style={HomeStyles.priceContainer}>
+            <Text style={HomeStyles.h1}>$799</Text>
+
+            <View style={HomeStyles.iconContainer} >
+              <View style={[HomeStyles.iconItem, {backgroundColor: '#144273'}]}>
+                <Image source={require('../../assets/share_icon.png')} />
+              </View>
+
+              <LinearGradient
+                colors={['#2ED982', '#14A08D']}
+                start={{x: 1, y: 0}}
+                end={{x: 0, y: 0}}
+                style={HomeStyles.iconItem}
+              >
+                <Image source={require('../../assets/shopping_bag_icon.png')} />
+              </LinearGradient>
+            </View>
+          </View>
+
+        </View>
+
       </View>
     </View>
   )
--- src/styles/homeStyles.js
+++ src/styles/homeStyles.js
@@ -3,23 +3,110 @@ import { StyleSheet } from "react-native";
 export const HomeStyles = StyleSheet.create({
     container: {
         flex: 1,
-        width: 'auto'
+        width: 'auto',
+        backgroundColor: '#ffffff',
+        paddingHorizontal: 20
     },
+
+    // Header content
+
     headerContainer: {
-        display: 'flex',
         flexDirection: 'row',
         width: '100%',
         height: 80,
-        justifyContent: 'space-around',
+        justifyContent: 'space-between',
         alignItems: 'center',
     },
     nikeImg: {
-        width: 50,
-        height: 50,
+        width: 60,
+        height: 60,
     },
     personImg: {
         width: 40,
         height: 40,
         borderRadius: '50%'
-    }
+    },
+    navContainer: {
+        gap: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: 30
+    },
+    item: {
+        flexShrink: 1,
+        width: '100%',
+        height: 120,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20,
+        backgroundColor: '#F3F3F2',
+    },
+
+    imgItem: {
+        resizeMode: 'contain',
+        width: '100%'
+    },
+
+    // Main Content
+
+    mainContainer: {
+        width: 'auto',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    mainImgContainer: {
+        width: '100%',
+    },
+    mainImg: {
+        resizeMode: 'contain',
+        width: 'auto'
+        
+    },
+    info: {
+        gap: 10,
+    },
+    h1: {
+        fontFamily: 'HoltwoodOneSC_400Regular',
+        color: '#144273',
+        fontSize: 30,
+    },
+    h2: {
+        fontFamily: 'IstokWeb_400Regular',
+        color: '#144273',
+        fontSize: 20,
+        fontWeight: 600,
+    },
+    p: {
+        fontFamily: 'IstokWeb_400Regular',
+        color: '#144273',
+        lineHeight: '1.5rem'
+    },
+
+    // Footer Content
+
+    footerContainer: {
+        width: '100%',
+        height: 'auto',
+        marginVertical: 20,
+    },
+    priceContainer: {
+        flexShrink: 1,
+        display: 'flex',
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    iconContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 20,
+    },
+    iconItem: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: '50%',
+        padding: 10
+    },
 })
