c50731affb019e4d399611f64cd9fe7c70d5b888
AlexandreBnovais <157971881+AlexandreBnovais@users.noreply.github.com>
2025-09-17 02:11:29 -0300

    New Feature: SplshScreen

--- src/screens/splashScreen.js
+++ src/screens/splashScreen.js
@@ -1,10 +1,44 @@
-import React from 'react'
-import { View } from 'react-native';
+import {
+  View,
+  Image,
+  Text,
+  TouchableOpacity
+} from 'react-native';
+import { SplashStyles } from '../styles/splashStyles';
+import { LinearGradient } from 'expo-linear-gradient';
+import Feather from '@expo/vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 export function SplashScreen() {
-  return (
-    <View>
+  const Navigator = useNavigation();
 
-    </View>
+  return (
+    <LinearGradient
+      colors={['#14A08D', '#2ED982']}
+      start={{x: 1, y: 0}}
+      end={{x: 0, y: 1}}
+      style={SplashStyles.container}
+    >
+      <Image source={require('../../assets/logo_nike.svg')}/>
+
+      <View style={SplashStyles.main}>
+        <Image source={require('../../assets/tennis_2.png')} style={SplashStyles.imgPng}/>
+        
+        <View style={SplashStyles.textContainer}>
+          <Text style={SplashStyles.h1}>SUMMER COLLECTIONS</Text>
+          <Text style={[SplashStyles.h1, {color: '#fff'}]}>2022</Text>
+          <Text style={SplashStyles.p}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</Text>
+        </View>
+        
+      </View>
+      <View style={{alignItems: 'center'}}>
+        <TouchableOpacity onPress={() => Navigator.navigate('Login')} style={SplashStyles.button}>
+          <Text style={SplashStyles.textButton}>GET STARTED</Text>
+          <Feather name="arrow-right" size={24} color="#fff" />
+        </TouchableOpacity>
+      </View>
+      
+
+    </LinearGradient>
   )
 }
